bc5522ad52c47f89b41cbac836558ee7771daaf0
Deepak Sharma <ds0404043@gmail.com>
2026-07-27 13:49:13 +0530

    Refactor GTA launcher to standalone page

--- js/gta.js
+++ js/gta.js
@@ -1,7 +1,5 @@
 console.log("gta.js loaded");
 
-const fullscreenBtn = document.getElementById("fullscreenBtn");
-const gameContainer = document.getElementById("gameContainer");
 const playBtn = document.querySelector(".play-game");
 const launcherScreen = document.querySelector(".launcher-screen");
 
@@ -10,31 +8,7 @@ playBtn.addEventListener("click", () => {
     launcherScreen.classList.add("active");
 
     setTimeout(() => {
-
-        launcherScreen.classList.remove("active");
-
-        document.getElementById("gtaFrame").style.display = "block";
-
-    },2500);
-
-});
-
-fullscreenBtn.addEventListener("click",()=>{
-
-    const iframe=document.getElementById("gtaFrame");
-
-    if(iframe){
-
-        if(iframe.requestFullscreen){
-
-            iframe.requestFullscreen();
-
-        }else if(iframe.webkitRequestFullscreen){
-
-            iframe.webkitRequestFullscreen();
-
-        }
-
-    }
+        window.location.href = "games/gtavc/GTA_Vice_City.html";
+    }, 2500);
 
 });
